d63bb749081840197b497b1f3d19fdaf6b2c2345
Rupesh Adhikari <apabaad@yahoo.com>
2021-09-14 14:06:12 +1000

    initial setup complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      Ecommerce
+      <Button variant="primary">
+        <i class="fas fa-users"></i> User
+      </Button>
+    </div>
+  );
+}
+
+export default App;

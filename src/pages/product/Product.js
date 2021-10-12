98547438a73bf38757739df7a3e24b7feda1cf71
Rupesh Adhikari <apabaad@yahoo.com>
2021-10-13 08:40:01 +1100

    product UI added

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import AdminLayout from '../Layout/AdminLayout';
-
+import { Button } from 'react-bootstrap';
+import { ListTable } from '../../components/tables/ListTable';
 const Product = () => {
   return (
     <AdminLayout>
-      <div>
-        <h1>Welcome to Product!</h1>
+      <div className="p-2">
+        <h1>Product</h1>
+        <hr />
+        <div className="text-end">
+          <Button variant="primary" className="text-end">
+            <i class="fas fa-plus"></i> Add new product
+          </Button>
+        </div>
+        <hr />
+        <div className="product-list">
+          <ListTable />
+        </div>
       </div>
     </AdminLayout>
   );
